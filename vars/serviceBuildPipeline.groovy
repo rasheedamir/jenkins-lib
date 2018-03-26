64c19987640a53b3a52f810a96ef4408e609df8f
Gábor Takács <gabor.takacs@semcon.com>
2018-03-26 15:49:37 +0200

    Fix sleep time while waiting for lock

--- vars/serviceBuildPipeline.groovy
+++ vars/serviceBuildPipeline.groovy
@@ -61,12 +61,12 @@ def call(body) {
                         stash includes: 'service-deployment.yaml', name: 'manifest'
                     }
                 }
-                
+
                 stage("Acquire lock on mock") {
                     lock = tryLock("mock", env.JOB_NAME, (20 + 5) * 60, 4, buildVersion)
                     while (lock == "") {
                         println "Waiting for lock"
-                        sleep(1000)
+                        sleep 4
                         loc = tryLock("mock", env.JOB_NAME, (20 + 5) * 60, 4, buildVersion)
                     }
                 }
