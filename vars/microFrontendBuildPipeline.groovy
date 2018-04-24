3038de76d57e267bec7dc27c2f4ec15f397464b6
Gábor Takács <gabor.takacs@semcon.com>
2018-04-24 09:41:19 +0200

    Add tagging

--- vars/microFrontendBuildPipeline.groovy
+++ vars/microFrontendBuildPipeline.groovy
@@ -1,4 +1,5 @@
 def call(body) {
+    def credentialId = 'dd_ci'
 
     def config = [:]
 
@@ -8,11 +9,12 @@ def call(body) {
 
     def version
     def name
+    def scmVars
 
     dockerNode(dockerImage: 'stakater/frontend-tools:latest') {
         container(name: 'docker') {
             stage("Checkout") {
-                checkout scm
+                scmVars = checkout scm
                 def js_package = readJSON file: 'package.json'
                 def version_old = js_package.version.tokenize(".")
                 version = "${version_old[0]}.${version_old[1]}.${env.BUILD_NUMBER}"
@@ -57,12 +59,23 @@ def call(body) {
                 }
             }
 
-                stage("Publish to nexus") {
-                    withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
-                                      variable: 'NEXUS_NPM_AUTH']]) {
-                        sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} npm publish"
-                    }
+            stage("Tag") {
+                withCredentials([usernamePassword(credentialsId: credentialId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                    sh """
+                        git config user.name "${scmVars.GIT_AUTHOR_NAME}" # TODO move to git config
+                        git config user.email "${scmVars.GIT_AUTHOR_EMAIL}"
+                        git tag -am "By ${currentBuild.projectName}" v${version}
+                        git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${scmVars.GIT_URL.substring(8)} v${version}
+                    """
                 }
+            }
+
+            stage("Publish to nexus") {
+                withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
+                                  variable: 'NEXUS_NPM_AUTH']]) {
+                    sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} npm publish"
+                }
+            }
 
             stage("Upload to S3") {
                 s3Upload(file: 'lib/', bucket: "${params.BUCKET}", path: "${name}/${version}/")
