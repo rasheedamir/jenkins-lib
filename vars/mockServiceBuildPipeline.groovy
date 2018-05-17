a3c6d70f71d1e0a70549f835a947f701ead09c5c
mjxkxx <mariam.jobe@scania.com>
2018-05-17 09:02:45 +0200

    print slack message

--- vars/mockServiceBuildPipeline.groovy
+++ vars/mockServiceBuildPipeline.groovy
@@ -22,104 +22,103 @@ def call(Map parameters = [:], body) {
     def dockerRepo = params.DOCKER_URL
     def serviceName = parameters.get('serviceName')
     assert serviceName != null: "Build fails because serviceName missing"
+    try {
+//    podTemplate(envVars: [envVar(key: 'FABRIC8_DOCKER_REGISTRY_SERVICE_HOST', value: dockerRepo),
+//                          envVar(key: 'FABRIC8_DOCKER_REGISTRY_SERVICE_PORT', value: '443')],
+//            volumes: [
+//                    secretVolume(secretName: "${kubeConfig}", mountPath: '/home/jenkins/.kube'),
+//            ])
+//            {
+//                clientsK8sNode(clientsImage: 'stakater/docker-with-git:17.10') {
+//                    stage("Checkout") {
+//                        scmVars = checkout scm
+//                        int version_last = sh(
+//                                script: "git tag | awk -F. 'BEGIN {print \"-1\"} /v${versionPrefix}/{print \$3}' | sort -g  | tail -1",
+//                                returnStdout: true
+//                        )
+//                        buildVersion = "${versionPrefix}.${version_last + 1}"
+//                        currentBuild.displayName = "${buildVersion}"
+//                    }
+//
+//                    stage('Build Release') {
+//                        echo 'NOTE: running pipelines for the first time will take longer as build and base docker images are pulled onto the node'
+//
+//                        container('clients') {
+//                            newImageName = "${dockerRepo}/${serviceName}:${buildVersion}"
+//                            sh "docker build -t ${newImageName} ."
+//                            withCredentials([usernamePassword(credentialsId: credentialId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+//                                sh """
+//                                    git config user.name "${scmVars.GIT_AUTHOR_NAME}"
+//                                    git config user.email "${scmVars.GIT_AUTHOR_EMAIL}"
+//                                    git tag -am "By ${currentBuild.projectName}" v${buildVersion}
+//                                    git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${scmVars.GIT_URL.substring(8)} v${buildVersion}
+//                                """
+//                            }
+//                            sh "docker push ${newImageName}"
+//                        }
+//
+//                        rc = getDeploymentResourcesK8s {
+//                            projectName = serviceName
+//                            port = 8080
+//                            label = 'node'
+//                            version = buildVersion
+//                            imageName = newImageName
+//                            dockerRegistrySecret = 'docker-registry-secret'
+//                            readinessProbePath = "/readiness"
+//                            livenessProbePath = "/health"
+//                            ingressClass = "external-ingress"
+//                            resourceRequestCPU = "20m"
+//                            resourceRequestMemory = "200Mi"
+//                            resourceLimitCPU = "100m"
+//                            resourceLimitMemory = "1100Mi"
+//                        }
+//
+//                        writeFile file: "deployment.yaml", text: rc
+//
+//                        stash name: "manifest", includes: "deployment.yaml"
+//                    }
+//
+//                    mavenNode(mavenImage: 'maven:3.5-jdk-8') {
+//                        container(name: 'maven') {
+//                            stage("Upload to nexus") {
+//                                unstash "manifest"
+//                                sh """
+//                                mvn deploy:deploy-file \
+//                                    -Durl=https://${mavenRepo}/repository/maven-releases \
+//                                    -DrepositoryId=nexus \
+//                                    -DgroupId=com.scania.dd \
+//                                    -DartifactId=${serviceName} \
+//                                    -Dversion=${buildVersion} \
+//                                    -Dpackaging=yml \
+//                                    -Dclassifier=kubernetes \
+//                                    -Dfile=deployment.yaml
+//                                """
+//                                stash name: "manifest", includes: "deployment.yaml"
+//                            }
+//                        }
+//                    }
+//
+//                    clientsNode {
+//                        stage("Deploy") {
+//                            echo "Deploying project ${serviceName} image version: ${buildVersion} yaml version: ${buildVersion}"
+//                            unstash "manifest"
+//                            container(name: 'clients') {
+//                                sh "kubectl apply  -n=${nameSpace} -f deployment.yaml"
+//                                sh "kubectl rollout status deployment/${serviceName} -n=${nameSpace}"
+//                            }
+//                        }
+//                    }
+//
+//
+//                }
+//            }
+        error("Build failed")
+    }
+    finally {
+        echo "Post-Build currentResult: ${currentBuild.currentResult}"
+        if (currentBuild.currentResult =='FAILURE'){
+            notifySlack()
+        }
 
-    podTemplate(envVars: [envVar(key: 'FABRIC8_DOCKER_REGISTRY_SERVICE_HOST', value: dockerRepo),
-                          envVar(key: 'FABRIC8_DOCKER_REGISTRY_SERVICE_PORT', value: '443')],
-            volumes: [
-                    secretVolume(secretName: "${kubeConfig}", mountPath: '/home/jenkins/.kube'),
-            ])
-            {
-                clientsK8sNode(clientsImage: 'stakater/docker-with-git:17.10') {
-                    stage("Checkout") {
-                        scmVars = checkout scm
-                        int version_last = sh(
-                                script: "git tag | awk -F. 'BEGIN {print \"-1\"} /v${versionPrefix}/{print \$3}' | sort -g  | tail -1",
-                                returnStdout: true
-                        )
-                        buildVersion = "${versionPrefix}.${version_last + 1}"
-                        currentBuild.displayName = "${buildVersion}"
-                    }
-
-                    stage('Build Release') {
-                        echo 'NOTE: running pipelines for the first time will take longer as build and base docker images are pulled onto the node'
-
-                        container('clients') {
-                            newImageName = "${dockerRepo}/${serviceName}:${buildVersion}"
-                            sh "docker build -t ${newImageName} ."
-                            withCredentials([usernamePassword(credentialsId: credentialId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                                sh """
-                                    git config user.name "${scmVars.GIT_AUTHOR_NAME}"
-                                    git config user.email "${scmVars.GIT_AUTHOR_EMAIL}"
-                                    git tag -am "By ${currentBuild.projectName}" v${buildVersion}
-                                    git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${scmVars.GIT_URL.substring(8)} v${buildVersion}
-                                """
-                            }
-                            sh "docker push ${newImageName}"
-                        }
-
-                        rc = getDeploymentResourcesK8s {
-                            projectName = serviceName
-                            port = 8080
-                            label = 'node'
-                            version = buildVersion
-                            imageName = newImageName
-                            dockerRegistrySecret = 'docker-registry-secret'
-                            readinessProbePath = "/readiness"
-                            livenessProbePath = "/health"
-                            ingressClass = "external-ingress"
-                            resourceRequestCPU = "20m"
-                            resourceRequestMemory = "200Mi"
-                            resourceLimitCPU = "100m"
-                            resourceLimitMemory = "1100Mi"
-                        }
-
-                        writeFile file: "deployment.yaml", text: rc
-
-                        stash name: "manifest", includes: "deployment.yaml"
-                    }
-
-                    mavenNode(mavenImage: 'maven:3.5-jdk-8') {
-                        container(name: 'maven') {
-                            stage("Upload to nexus") {
-                                unstash "manifest"
-                                sh """
-                                mvn deploy:deploy-file \
-                                    -Durl=https://${mavenRepo}/repository/maven-releases \
-                                    -DrepositoryId=nexus \
-                                    -DgroupId=com.scania.dd \
-                                    -DartifactId=${serviceName} \
-                                    -Dversion=${buildVersion} \
-                                    -Dpackaging=yml \
-                                    -Dclassifier=kubernetes \
-                                    -Dfile=deployment.yaml
-                                """
-                                stash name: "manifest", includes: "deployment.yaml"
-                            }
-                        }
-                    }
-
-                    clientsNode {
-                        stage("Deploy") {
-                            echo "Deploying project ${serviceName} image version: ${buildVersion} yaml version: ${buildVersion}"
-                            unstash "manifest"
-                            container(name: 'clients') {
-                                sh "kubectl apply  -n=${nameSpace} -f deployment.yaml"
-                                sh "kubectl rollout status deployment/${serviceName} -n=${nameSpace}"
-                            }
-                        }
-                    }
-                    try {
-                        echo "Hello world"
-                        //error("Build failed because of this and that..")
-                    }
-                    finally {
-                        echo "Post-Build currentResult: ${currentBuild.currentResult}"
-                        if (currentBuild.currentResult =='SUCCESS'){
-                            notifySlack()
-                        }
-
-                    }
-
-                }
-            }
+    }
 }
