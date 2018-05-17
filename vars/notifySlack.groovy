df1296e6b4c6f77bccf962af4b03194c21cb71ad
mjxkxx <mariam.jobe@scania.com>
2018-05-17 08:12:01 +0200

    wip - version nr

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -2,7 +2,7 @@
 def call() {
             slackSend channel: '#jenkinstest',
                     color: 'danger',
-                    message: "Build FAILED -  Job: ${env.JOB_NAME} BuildNr: # currentBuild.displayName (<${env.BUILD_URL}|Go to build>)",
+                    message: "Build FAILED -  Job: ${env.JOB_NAME} BuildNr: #${currentBuild.displayName} (<${env.BUILD_URL}|Go to build>)",
                     teamDomain: 'digitialdealer',
                     token: '8vmDPD2QkYIX0pu3Rcf3dA4i'
 
