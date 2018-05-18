c7da2ab9673ffe90396076ba93a4430f98fc7e42
mjxkxx <mariam.jobe@scania.com>
2018-05-18 10:32:43 +0200

    wip - execute body

--- vars/withNotifySlack.groovy
+++ vars/withNotifySlack.groovy
@@ -4,31 +4,33 @@ import jenkins.model.Jenkins
 def call(body) {
     def credentialsId = 'slack_token'
     def jenkins_creds = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0]
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+
     try {
         body()
-        error("Build failed")
+       // error("Build failed")
     } catch (e) {
         currentBuild.result = "FAILED"
         throw e
 
     } finally {
-
-
-        String slacktoken = jenkins_creds.getStore().getDomains().findResult { domain ->
+        String slackToken = jenkins_creds.getStore().getDomains().findResult { domain ->
             jenkins_creds.getCredentials(domain).findResult { credential ->
                 if (credentialsId.equals(credential.id)) {
                     credential.getSecret()
                 }
             }
         }
-        println "Slack token credentials: ${slacktoken}"
 
         if (currentBuild.currentResult == 'FAILURE') {
             slackSend channel: '#redlamp',
                     color: 'danger',
                     message: "Build FAILED -  Job: ${env.JOB_NAME},  BuildNr: ${currentBuild.displayName} (<${env.BUILD_URL}|Go to build>)",
                     teamDomain: 'digitialdealer',
-                    token: "${slacktoken}"
+                    token: "${slackToken}"
 
         }
     }
