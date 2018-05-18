3386e972d0d5c83c665c0d64caa061521e511bf3
mjxkxx <mariam.jobe@scania.com>
2018-05-18 09:32:25 +0200

    wip

--- vars/withNotifySlack.groovy
+++ vars/withNotifySlack.groovy
@@ -7,19 +7,20 @@ def call(body) {
     body.delegate = config
 
     try {
-       body()
+      // body()
+        error("Build failed")
     } catch (e) {
         currentBuild.result = "FAILED"
         throw e
 
     } finally {
-        if (currentBuild.currentResult == 'SUCCESS') {
+        if (currentBuild.currentResult == 'FAILURE') {
             withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
                 slackSend channel: '#redlamp',
                         color: 'danger',
                         message: "Build FAILED -  Job: ${env.JOB_NAME},  BuildNr: ${currentBuild.displayName} (<${env.BUILD_URL}|Go to build>)",
                         teamDomain: 'digitialdealer',
-                        token: $token
+                        token: ${token}
             }
         }
     }
