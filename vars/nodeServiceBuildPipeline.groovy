4796168554e88cc29c4dbb93a553ed302107d755
Gábor <11243359+gabor-nt@users.noreply.github.com>
2018-08-13 14:21:00 +0200

    Add testing and deployment to node.js pipeline (#29)
    Read version prefix from package
    Deploy via jobs
    Lock with function

--- vars/nodeServiceBuildPipeline.groovy
+++ vars/nodeServiceBuildPipeline.groovy
@@ -5,114 +5,160 @@ def call(configMap) {
     def mavenRepo = params.MAVEN_REPO
     def credentialId = 'dd_ci'
 
-    try {
-        versionPrefix = VERSION_PREFIX
-    } catch (Throwable ignored) {
-        versionPrefix = "1.4"
-    }
-
-    def newVersion
+    def buildVersion
     def scmVars
     def newImageName
-    def serviceName
+    def project
 
-    podTemplate(envVars: [envVar(key: 'FABRIC8_DOCKER_REGISTRY_SERVICE_HOST', value: dockerUrl),
-                          envVar(key: 'FABRIC8_DOCKER_REGISTRY_SERVICE_PORT', value: '443')],
-            volumes: [
-                    secretVolume(secretName: "${kubeConfig}", mountPath: '/home/jenkins/.kube'),
-                    secretVolume(secretName: 'digitaldealer-service-secret', mountPath: '/etc/secrets/service-secret')
-            ])
-            {
-                clientsK8sNode(clientsImage: 'stakater/pipeline-tools:1.11.0') {
+    podTemplate(volumes: [secretVolume(secretName: "${kubeConfig}", mountPath: '/home/jenkins/.kube')]) {
+        clientsK8sNode(clientsImage: 'stakater/pipeline-tools:1.11.0') {
+            stage("Checkout") {
+                scmVars = checkout scm
 
-                    stage("Checkout") {
-                        scmVars = checkout scm
+                def js_package = readJSON file: 'package.json'
+                project = js_package.name
+                boolean containsData = project?.trim()
 
-                        def js_package = readJSON file: 'package.json'
-                        serviceName = js_package.name
-                        boolean containsData = serviceName?.trim()
+                if (!containsData) {
+                    error("Property 'name' in package.json cannot be found")
+                }
 
-                        if (!containsData) {
-                            error("Property 'name' in package.json cannot be found")
-                        }
+                def version_base = js_package.version.tokenize(".")
+                int version_last = sh(
+                        script: "git tag | awk -F. 'BEGIN {print \"-1\"} /v${version_base[0]}.${version_base[1]}/{print \$3}' | sort -g  | tail -1",
+                        returnStdout: true
+                )
+                buildVersion = "${version_base[0]}.${version_base[1]}.${version_last + 1}"
+                currentBuild.displayName = "${buildVersion}"
+            }
 
-                        int version_last = sh(
-                                script: "git tag | awk -F. 'BEGIN {print \"-1\"} /v${versionPrefix}/{print \$3}' | sort -g  | tail -1",
-                                returnStdout: true
-                        )
-                        newVersion = "${versionPrefix}.${version_last + 1}"
-                        currentBuild.displayName = "${newVersion}"
-                    }
+            stage('Build Release') {
+                echo 'NOTE: running pipelines for the first time will take longer as build and base docker images are pulled onto the node'
+                if (!fileExists('Dockerfile')) {
+                    writeFile file: 'Dockerfile', text: 'FROM node:5.3-onbuild'
+                }
 
-                    stage('Build Release') {
-                        echo 'NOTE: running pipelines for the first time will take longer as build and base docker images are pulled onto the node'
-                        if (!fileExists('Dockerfile')) {
-                            writeFile file: 'Dockerfile', text: 'FROM node:5.3-onbuild'
-                        }
-
-                        container('clients') {
-                            newImageName = "${dockerUrl}/${serviceName}:${newVersion}"
-                            sh "docker build --network=host -t ${newImageName} ."
-                            withCredentials([usernamePassword(credentialsId: credentialId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                                sh """
+                container('clients') {
+                    newImageName = "${dockerUrl}/${project}:${buildVersion}"
+                    sh "docker build --network=host -t ${newImageName} ."
+                    withCredentials([usernamePassword(credentialsId: credentialId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                        sh """
                                 git config user.name "${scmVars.GIT_AUTHOR_NAME}"
                                 git config user.email "${scmVars.GIT_AUTHOR_EMAIL}"
-                                git tag -am "By ${currentBuild.projectName}" v${newVersion}
-                                git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${scmVars.GIT_URL.substring(8)} v${newVersion}
+                                git tag -am "By ${currentBuild.projectName}" v${buildVersion}
+                                git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${scmVars.GIT_URL.substring(8)} v${
+                            buildVersion
+                        }
                             """
-                            }
-                            sh "docker push ${newImageName}"
-                        }
-
-                        rc = getDeploymentResourcesK8s {
-                            projectName = serviceName
-                            port = 8080
-                            label = 'node'
-                            version = newVersion
-                            imageName = newImageName
-                            dockerRegistrySecret = 'docker-registry-secret'
-                            readinessProbePath = "/readiness"
-                            livenessProbePath = "/health"
-                            ingressClass = "external-ingress"
-                            configMapToMount = configMap
-                        }
-
-
-                        writeFile file: "deployment.yaml", text: rc
-
-
-
-                        stash name: "manifest", includes: "deployment.yaml"
                     }
+                    sh "docker push ${newImageName}"
+                }
 
-                    mavenNode(mavenImage: 'maven:3.5-jdk-8') {
-                        container(name: 'maven') {
-                            stage("Upload to nexus") {
-                                unstash "manifest"
-                                sh """
+                rc = getDeploymentResourcesK8s {
+                    projectName = project
+                    port = 8080
+                    label = 'node'
+                    version = buildVersion
+                    imageName = newImageName
+                    dockerRegistrySecret = 'docker-registry-secret'
+                    readinessProbePath = "/readiness"
+                    livenessProbePath = "/health"
+                    ingressClass = "external-ingress"
+                    configMapToMount = configMap
+                }
+                writeFile file: "deployment.yaml", text: rc
+                stash name: "manifest", includes: "deployment.yaml"
+            }
+
+            mavenNode(mavenImage: 'maven:3.5-jdk-8') {
+                container(name: 'maven') {
+                    stage("Upload to nexus") {
+                        unstash "manifest"
+                        sh """
                                     mvn deploy:deploy-file \
                                         -Durl=https://${mavenRepo}/repository/maven-releases \
                                         -DrepositoryId=nexus \
                                         -DgroupId=com.scania.dd \
-                                        -DartifactId=${serviceName} \
-                                        -Dversion=${newVersion} \
+                                        -DartifactId=${project} \
+                                        -Dversion=${buildVersion} \
                                         -Dpackaging=yml \
                                         -Dclassifier=kubernetes \
                                         -Dfile=deployment.yaml
                                 """
-                                stash name: "manifest", includes: "deployment.yaml"
-                            }
-                        }
-                    }
-
-                    stage("Deploy") {
-                        echo "Deploying project ${serviceName} version: ${newVersion}"
-                        unstash "manifest"
-                        container(name: 'clients') {
-                            sh "kubectl apply  -n=${nameSpace} -f deployment.yaml"
-                            sh "kubectl rollout status deployment/${serviceName} -n=${nameSpace}"
-                        }
+                        stash name: "manifest", includes: "deployment.yaml"
                     }
                 }
             }
+
+            String lockName = "${env.JOB_NAME}-${env.BUILD_NUMBER}"
+
+            def workspace = env.WORKSPACE
+            withLockOnMockEnvironment(lockName: lockName) {
+                def prevVersion = ""
+                stage("Saving old service version") {
+                    container(name: 'clients') {
+                        try {
+                            prevVersion = sh(script: "kubectl -n=mock get service/${project} -o jsonpath='{.metadata.labels.version}' 2>${workspace}/serr.txt", returnStdout: true).toString().trim()
+                            echo "Old version is: ${prevVersion}"
+                        } catch (err) {
+                            echo "Reading old version failed: $err"
+                            def errorMessage = readFile "${workspace}/serr.txt"
+                            echo "Message: $errorMessage"
+                            if (errorMessage.contains("(NotFound)")) {
+                                echo "Probably this is the first deployment"
+                            } else {
+                                echo "We did not expect this!"
+                                throw err
+                            }
+                        }
+                    }
+                }
+
+                try {
+                    stage("Deploy to mock") {
+                        build job: "${project}-mock-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
+                    }
+                    timeout(20) {
+                        mavenNode(mavenImage: 'stakater/chrome:67') {
+                            container(name: 'maven') {
+                                try {
+                                    stage("checking out mock tests") {
+                                        git url: 'https://gitlab.com/digitaldealer/systemtest2.git',
+                                                credentialsId: 'dd_ci',
+                                                branch: 'master'
+                                    }
+
+                                    stage("running mock tests") {
+                                        sh 'chmod +x mvnw'
+                                        sh './mvnw clean test -Dbrowser=chrome -Dheadless=true -DsuiteXmlFile=smoketest-mock.xml'
+                                    }
+                                } finally {
+                                    zip zipFile: 'output.zip', dir: 'target', archive: true
+                                    archiveArtifacts artifacts: 'target/screenshots/*', allowEmptyArchive: true
+                                    junit 'target/surefire-reports/*.xml'
+                                }
+                            }
+                        }
+                    }
+                } catch (err) {
+                    if (prevVersion != "") {
+//                              Rolling back only rolls back the deployment, the service stays
+//                              If we reapply an old manifest, the service will be correct, the replica set will be reused, but the deployment/replica set will have a new revision
+                        build job: "${project}-dev-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: prevVersion]]
+                    } else {
+                        echo "There were test failures, but there was no previous version, not rolling back mock"
+                    }
+                    throw err
+                }
+            }
+
+            stage("Deploy to dev") {
+                build job: "${project}-dev-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
+            }
+
+            stage('Deploy to prod') {
+            build job: "${project}-prod-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
+            }
+        }
+    }
 }
