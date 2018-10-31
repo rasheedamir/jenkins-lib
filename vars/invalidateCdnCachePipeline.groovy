beab4992489188b42972b70f43fb6ca4a554bc1b
mrpuurple <nichlas.eriksson@gmail.com>
2018-10-31 13:30:34 +0100

    Add cdn invalidate cache lib (#53)
    * new version of build pipeline
    
    * new library created
    
    * new library updateK8sClusterPipeline
    
    * changed call function argument from Map config to body
    
    * removed dry run
    
    * changed to stakater pipeline-tools docker image
    
    * Added dryrun step to get the output from Kops
    
    * added invalidateCdnCachePipeline.groovy
    
    * added aws assuming account variable

---
+++ vars/invalidateCdnCachePipeline.groovy
@@ -0,0 +1,44 @@
+// Requiers the CDN_ID and ASSUMING_ACCOUNT variables to be set
+def call(body){
+
+    clientsK8sNode(clientsImage: 'stakater/pipeline-tools:v1.16.2') {
+        container('clients') {
+
+            stage('Checkout i18n-locales repo') {
+
+                sh """
+                    mkdir workdir
+                """
+    
+                dir('workdir') {
+                    git (
+                        url: "https://gitlab.com/digitaldealer/i18n-locales.git",
+                        credentialsId: 'dd_ci'
+                    )
+                }
+
+            }
+
+            stage('Creating CDN cache invalidation') {
+
+                sh """#!/bin/bash
+                    cd workdir
+                    BATCH=`git diff | git --no-pager show HEAD^2  --name-only | awk -v ORS=' ' -F'/' '/.json\$/ {print "/i18n-locales/"\$(NF-1)"/"\$NF}'`
+                    echo "Paths: \${BATCH}"
+                    echo "Distribustion ID: ${CDN_ID}"
+                    mkdir ${HOME}.aws
+                    cat <<- EOF > ${HOME}.aws/config
+[profile cdn-profile]
+role_arn = arn:aws:iam::${ASSUMING_ACCOUNT}:role/CDNRole
+credential_source = Ec2InstanceMetadata
+EOF
+                    cat ${HOME}.aws/config
+                    echo "aws --profile cdn-profile cloudfront create-invalidation --distribution-id ${CDN_ID} --paths \${BATCH}"
+                """
+            }
+
+        }
+
+    }
+
+}
