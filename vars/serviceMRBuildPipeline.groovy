72a77df969345eb5a69a0d2cc4b290b3a0ebc43e
jokarls <jonas.lmk@gmail.com>
2018-11-13 14:19:45 +0100

    Update serviceMRBuildPipeline.groovy
    added gitlab notifications

--- vars/serviceMRBuildPipeline.groovy
+++ vars/serviceMRBuildPipeline.groovy
@@ -13,6 +13,8 @@ def call(body) {
 
     def onlyMock = config.onlyMock ?: false
     def onlyMockDeploy = mergeRequestBuild || onlyMock
+    
+    echo "onlyMockDeploy: ${onlyMockDeploy}"
 
     def project
     def buildVersion
@@ -20,107 +22,114 @@ def call(body) {
 
     timestamps {
         withSlackNotificatons() {
-            podTemplate(name: 'sa-secret',
-                    serviceAccount: 'digitaldealer-serviceaccount',
-                    envVars: [envVar(key: 'KUBERNETES_MASTER', value: 'https://kubernetes.default:443')],
-                    volumes: [
-                            secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/home/jenkins/.m2'),
-                            persistentVolumeClaim(claimName: 'jenkins-m2-cache', mountPath: '/home/jenkins/.mvnrepository'),
-                            secretVolume(secretName: "${kubeConfig}", mountPath: '/home/jenkins/.kube'),
-                            secretVolume(secretName: 'digitaldealer-service-secret', mountPath: '/etc/secrets/service-secret')
-                    ])
-                    {
-                        mavenNode(maven: 'stakater/pipeline-tools:1.11.0') {
-                            container(name: 'maven') {
-                                // Ensure "jenkins" user is the owner of mounted Maven repository
-                                // As the default owner in the volume would be "root" unless changed once
-                                stage("Change Ownership") {
-                                    def mvnRepository = "/home/jenkins/.mvnrepository"
-                                    // Run chown only if the directory is not already owned by the jenkins user
-                                    sh """
+            gitlabBuilds(builds: ["build", "System test"]) {
+                podTemplate(
+                        name: 'sa-secret',
+                        serviceAccount: 'digitaldealer-serviceaccount',
+                        envVars: [envVar(key: 'KUBERNETES_MASTER', value: 'https://kubernetes.default:443')],
+                        volumes: [
+                                secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/home/jenkins/.m2'),
+                                persistentVolumeClaim(claimName: 'jenkins-m2-cache', mountPath: '/home/jenkins/.mvnrepository'),
+                                secretVolume(secretName: "${kubeConfig}", mountPath: '/home/jenkins/.kube'),
+                                secretVolume(secretName: 'digitaldealer-service-secret', mountPath: '/etc/secrets/service-secret')
+                        ])
+                        {
+                            mavenNode(maven: 'stakater/pipeline-tools:1.11.0') {
+                                container(name: 'maven') {
+                                    // Ensure "jenkins" user is the owner of mounted Maven repository
+                                    // As the default owner in the volume would be "root" unless changed once
+                                    stage("Change Ownership") {
+                                        def mvnRepository = "/home/jenkins/.mvnrepository"
+                                        // Run chown only if the directory is not already owned by the jenkins user
+                                        sh """
                                         MVN_DIR_OWNER=\$(ls -ld ${mvnRepository} | awk '{print \$3}')
                                         if [ \${MVN_DIR_OWNER} != '10000' ];
                                         then
                                             chown 10000 -R /home/jenkins/.mvnrepository
                                         fi
                                     """
+                                    }
                                 }
                             }
-                        }
 
-                        mavenNode(
-                                mavenImage: 'stakater/maven-jenkins:3.5.4-0.6',
-                                javaOptions: '-Duser.home=/home/jenkins -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dsun.zip.disableMemoryMapping=true -XX:+UseParallelGC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90',
-                                mavenOpts: '-Duser.home=/home/jenkins -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn') {
+                            mavenNode(
+                                    mavenImage: 'stakater/maven-jenkins:3.5.4-0.6',
+                                    javaOptions: '-Duser.home=/home/jenkins -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dsun.zip.disableMemoryMapping=true -XX:+UseParallelGC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90',
+                                    mavenOpts: '-Duser.home=/home/jenkins -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn') {
 
-                            container(name: 'maven') {
+                                container(name: 'maven') {
 
-                                stage("checkout") {
-                                    scmVars = checkout scm
-                                    def pom = readMavenPom file: 'pom.xml'
-                                    project = pom.artifactId
+                                    stage("checkout") {
+                                        scmVars = checkout scm
+                                        def pom = readMavenPom file: 'pom.xml'
+                                        project = pom.artifactId
 
-                                    if (mergeRequestBuild) {
-                                        def branchName = env.gitlabSourceBranch
-                                        buildVersion = "${branchName}-${currentBuild.number}"
-                                    } else {
-                                        def versionPrefix = config.VERSION_PREFIX ?: "1.4"
-                                        int version_last = sh(
-                                                script: "git tag | awk -F. 'BEGIN {print \"-1\"} /v${versionPrefix}/{print \$3}' | sort -g  | tail -1",
-                                                returnStdout: true
-                                        )
-                                        buildVersion = "${versionPrefix}.${version_last + 1}"
+                                        if (mergeRequestBuild) {
+                                            def branchName = env.gitlabSourceBranch
+                                            buildVersion = "${branchName}-${currentBuild.number}"
+                                        } else {
+                                            def versionPrefix = config.VERSION_PREFIX ?: "1.4"
+                                            int version_last = sh(
+                                                    script: "git tag | awk -F. 'BEGIN {print \"-1\"} /v${versionPrefix}/{print \$3}' | sort -g  | tail -1",
+                                                    returnStdout: true
+                                            )
+                                            buildVersion = "${versionPrefix}.${version_last + 1}"
+                                        }
+
+                                        currentBuild.displayName = "${buildVersion}"
                                     }
 
-                                    currentBuild.displayName = "${buildVersion}"
-                                }
-
-                                stage('build') {
-                                    sh "git checkout -b ${env.JOB_NAME}-${buildVersion}"
-                                    sh "mvn org.codehaus.mojo:versions-maven-plugin:2.2:set -U -DnewVersion=${buildVersion}"
-                                    if (!mergeRequestBuild) {
-                                        withCredentials([usernamePassword(credentialsId: credentialId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                                            sh """
+                                    gitlabCommitStatus("build") {
+                                        stage('build') {
+                                            sh "git checkout -b ${env.JOB_NAME}-${buildVersion}"
+                                            sh "mvn org.codehaus.mojo:versions-maven-plugin:2.2:set -U -DnewVersion=${buildVersion}"
+                                            if (!mergeRequestBuild) {
+                                                withCredentials([usernamePassword(credentialsId: credentialId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                                                    sh """
                                             git config user.name "${scmVars.GIT_AUTHOR_NAME}"
                                             git config user.email "${scmVars.GIT_AUTHOR_EMAIL}"
                                             git tag -am "By ${currentBuild.projectName}" v${buildVersion}
                                             git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${scmVars.GIT_URL.substring(8)} v${buildVersion}
                                         """
+                                                }
+                                            }
+                                            sh "mvn deploy"
                                         }
                                     }
-                                    sh "mvn deploy"
+
+                                    stage('push docker image') {
+                                        sh "mvn fabric8:push -Ddocker.push.registry=${dockerRepo}"
+                                    }
+
                                 }
+                            }
 
-                                stage('push docker image') {
-                                    sh "mvn fabric8:push -Ddocker.push.registry=${dockerRepo}"
+                            gitlabCommitStatus("System test") {
+                                systemtestStage([
+                                        microservice: [
+                                                name   : project,
+                                                version: buildVersion
+                                        ]
+                                ])
+                            }
+
+                            stage("Deploy to dev") {
+                                if (onlyMockDeploy) {
+                                    echo "onlyMockDeploy flag enabled, skipping deployment to dev"
+                                } else {
+                                    build job: "${project}-dev-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
                                 }
+                            }
 
+                            stage('Deploy to prod') {
+                                if (onlyMockDeploy) {
+                                    echo "onlyMockDeploy flag enabled, skipping deployment to prod"
+                                } else {
+                                    build job: "${project}-prod-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
+                                }
                             }
                         }
-
-                        systemtestStage([
-                                microservice: [
-                                        name   : project,
-                                        version: buildVersion
-                                ]
-                        ])
-
-                        stage("Deploy to dev") {
-                            if (onlyMockDeploy) {
-                                echo "onlyMockDeploy flag enabled, skipping deployment to dev"
-                            } else {
-                                build job: "${project}-dev-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
-                            }
-                        }
-
-                        stage('Deploy to prod') {
-                            if (onlyMockDeploy) {
-                                echo "onlyMockDeploy flag enabled, skipping deployment to prod"
-                            } else {
-                                build job: "${project}-prod-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
-                            }
-                        }
-                    }
+            }
         }
     }
 }
