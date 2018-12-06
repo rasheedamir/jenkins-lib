4db5e1bcc4caa694e025eff8200219ce10fc1da2
emilhasteen <emil.hasteen@gmail.com>
2018-12-06 07:45:11 +0100

    Update nodeServicePipeline (#68)
    * Fix node service pipeline
    
    * Remove uncommented lines
    
    * Update after comments
    
    * Add log
    
    * move getVersion def
    
    * inline getversion call
    
    * remove the log
    
    * set version prefix if merge request build
    
    * take normal bj version and try to add prefix
    
    * extract prefix variable
    
    * add end quote
    
    * Remove the version-tag-prefix since valid for git tags only
    
    * add quotes
    
    * Dont set yarn version when building merge request builds
    
    * adjust to semver standard
    
    * adjust to semver standard
    
    * Delete unused method

--- vars/nodeServiceBuildPipeline.groovy
+++ vars/nodeServiceBuildPipeline.groovy
@@ -1,121 +1,144 @@
 def call(configMap) {
 
+    def credentialId = 'dd_ci'
+
     def kubeConfig = params.KUBE_CONFIG
     def dockerUrl = params.DOCKER_URL
     def mavenRepo = params.MAVEN_REPO
-    def credentialId = 'dd_ci'
 
     def buildVersion
     def scmVars
     def newImageName
     def project
 
+    def isMergeRequestBuild = params.IS_MERGE_REQUEST_BUILD ?: false
+    echo "isMergeRequestBuild: ${isMergeRequestBuild}"
+
+    assert !(isMergeRequestBuild && env.gitlabSourceBranch == null)
+
+    def branchName = isMergeRequestBuild ? env.gitlabSourceBranch : env.gitlabBranch ?: 'master'
+    def deployToDevAndProd = !(isMergeRequestBuild)
+    def mr_version_postfix = "-beta"
+
     timestamps {
-        podTemplate(volumes: [secretVolume(secretName: "${kubeConfig}", mountPath: '/home/jenkins/.kube')]) {
-            dockerNode(dockerImage: 'stakater/frontend-tools:0.1.0-8.12.0') {
-                container(name: 'docker') {
-                    stage("Build Package") {
-                        scmVars = checkout scm
+        withSlackNotificatons() {
+            try {
 
-                        def js_package = readJSON file: 'package.json'
-                        project = js_package.name
-                        boolean containsData = project?.trim()
+                gitlabBuilds(builds: ["Build", "System test"]) {
+                    podTemplate(volumes: [secretVolume(secretName: "${kubeConfig}", mountPath: '/home/jenkins/.kube')]) {
 
-                        if (!containsData) {
-                            error("Property 'name' in package.json cannot be found")
-                        }
+                        gitlabCommitStatus(name: "Build") {
 
-                        def version_base = js_package.version.tokenize(".")
-                        int version_last = sh(
-                                script: "git tag | awk -F. 'BEGIN {print \"-1\"} /v${version_base[0]}.${version_base[1]}/{print \$3}' | sort -g  | tail -1",
-                                returnStdout: true
-                        )
-                        buildVersion = "${version_base[0]}.${version_base[1]}.${version_last + 1}"
-                        currentBuild.displayName = "${buildVersion}"
+                            dockerNode(dockerImage: 'stakater/frontend-tools:0.1.0-8.12.0') {
 
-                        withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
-                                          variable: 'NEXUS_NPM_AUTH']]) {
-                            sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn version --no-git-tag-version --new-version ${buildVersion}"
-                            sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn install"
-                        }
+                                def js_package
 
-                        withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
-                                          variable: 'NEXUS_NPM_AUTH']]) {
-                            try {
-                                sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} JEST_JUNIT_OUTPUT=\"./unit-test-report.xml\" yarn test"
-                            } finally {
-                                junit 'unit-test-report.xml'
+                                container(name: 'docker') {
+                                    stage("checkout") {
+                                        scmVars = checkout([$class: 'GitSCM', branches: [[name: branchName]], userRemoteConfigs: scm.getUserRemoteConfigs()])
+                                        js_package = readJSON file: 'package.json'
+                                        project = js_package.name
+                                        def version_base = js_package.version.tokenize(".")
+
+                                        buildVersion = isMergeRequestBuild ? getBJVersion(version_base) + "${mr_version_postfix}" : getBJVersion(version_base)
+                                        currentBuild.displayName = "${buildVersion}"
+                                    }
+
+                                    stage("Build Package") {
+
+                                        project = js_package.name
+                                        boolean containsData = project?.trim()
+
+                                        if (!containsData) {
+                                            error("Property 'name' in package.json cannot be found")
+                                        }
+
+                                        withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
+                                                          variable: 'NEXUS_NPM_AUTH']]) {
+
+                                            sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn version --no-git-tag-version --new-version ${buildVersion}"
+                                            sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn install"
+                                        }
+
+                                        withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
+                                                          variable: 'NEXUS_NPM_AUTH']]) {
+                                            try {
+                                                sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} JEST_JUNIT_OUTPUT=\"./unit-test-report.xml\" yarn test"
+                                            } finally {
+                                                junit 'unit-test-report.xml'
+                                            }
+                                        }
+
+                                        withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
+                                                          variable: 'NEXUS_NPM_AUTH']]) {
+                                            sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn build"
+                                        }
+                                    }
+
+                                    stage("Tag and Publish Package") {
+                                        if (!isMergeRequestBuild) {
+                                            withCredentials([usernamePassword(credentialsId: credentialId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                                                sh """
+                                                git config user.name "${scmVars.GIT_AUTHOR_NAME}"
+                                                git config user.email "${scmVars.GIT_AUTHOR_EMAIL}"
+                                                git tag -am "By ${currentBuild.projectName}" v${buildVersion}
+                                                git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${scmVars.GIT_URL.substring(8)} v${
+                                                                    buildVersion
+                                                }
+                                            """
+                                            }
+                                        }
+
+                                        withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
+                                                          variable: 'NEXUS_NPM_AUTH']]) {
+                                            sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} npm publish"
+                                        }
+                                    }
+
+                                    stash name: "docker build input", includes: ".npmrc,Dockerfile"
+                                }
                             }
-                        }
 
-                        withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
-                                          variable: 'NEXUS_NPM_AUTH']]) {
-                            sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} yarn build"
-                        }
-                    }
+                            stage('Build Docker Image') {
+                                clientsK8sNode(clientsImage: 'stakater/pipeline-tools:1.16.0') {
+                                    unstash "docker build input"
+                                    echo 'NOTE: running pipelines for the first time will take longer as build and base docker images are pulled onto the node'
+                                    if (!fileExists('Dockerfile')) {
+                                        echo 'Creating dockerfile with onbuild base'
+                                        writeFile file: 'Dockerfile', text: 'FROM node:5.3-onbuild'
+                                    }
 
-                    stage("Tag and Publish Package") {
-                        withCredentials([usernamePassword(credentialsId: credentialId, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                            sh """
-                                git config user.name "${scmVars.GIT_AUTHOR_NAME}"
-                                git config user.email "${scmVars.GIT_AUTHOR_EMAIL}"
-                                git tag -am "By ${currentBuild.projectName}" v${buildVersion}
-                                git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${scmVars.GIT_URL.substring(8)} v${
-                                buildVersion
+                                    container('clients') {
+                                        newImageName = "${dockerUrl}/${project}:${buildVersion}"
+                                        withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
+                                                          variable: 'NEXUS_NPM_AUTH']]) {
+                                            sh "docker build --build-arg NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} --network=host -t ${newImageName} ."
+                                        }
+                                        sh "docker push ${newImageName}"
+                                    }
+
+                                    rc = getDeploymentResourcesK8s {
+                                        projectName = project
+                                        port = 8080
+                                        label = 'node'
+                                        version = buildVersion
+                                        imageName = newImageName
+                                        dockerRegistrySecret = 'docker-registry-secret'
+                                        readinessProbePath = "/readiness"
+                                        livenessProbePath = "/health"
+                                        ingressClass = "external-ingress"
+                                        configMapToMount = configMap
+                                    }
+                                    writeFile file: "deployment.yaml", text: rc
+                                    stash name: "manifest", includes: "deployment.yaml"
+                                }
                             }
-                            """
-                        }
 
-                        withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
-                                          variable: 'NEXUS_NPM_AUTH']]) {
-                            sh "NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} npm publish"
-                        }
-                    }
-
-                    stash name: "docker build input", includes: ".npmrc,Dockerfile"
-                }
-            }
-
-            stage('Build Docker Image') {
-                clientsK8sNode(clientsImage: 'stakater/pipeline-tools:1.16.0') {
-                    unstash "docker build input"
-                    echo 'NOTE: running pipelines for the first time will take longer as build and base docker images are pulled onto the node'
-                    if (!fileExists('Dockerfile')) {
-                        echo 'Creating dockerfile with onbuild base'
-                        writeFile file: 'Dockerfile', text: 'FROM node:5.3-onbuild'
-                    }
-
-                    container('clients') {
-                        newImageName = "${dockerUrl}/${project}:${buildVersion}"
-                        withCredentials([[$class  : 'StringBinding', credentialsId: 'NEXUS_NPM_AUTH',
-                                          variable: 'NEXUS_NPM_AUTH']]) {
-                            sh "docker build --build-arg NEXUS_NPM_AUTH=${NEXUS_NPM_AUTH} --network=host -t ${newImageName} ."
-                        }
-                        sh "docker push ${newImageName}"
-                    }
-
-                    rc = getDeploymentResourcesK8s {
-                        projectName = project
-                        port = 8080
-                        label = 'node'
-                        version = buildVersion
-                        imageName = newImageName
-                        dockerRegistrySecret = 'docker-registry-secret'
-                        readinessProbePath = "/readiness"
-                        livenessProbePath = "/health"
-                        ingressClass = "external-ingress"
-                        configMapToMount = configMap
-                    }
-                    writeFile file: "deployment.yaml", text: rc
-                    stash name: "manifest", includes: "deployment.yaml"
-                }
-            }
-
-            stage("Publish Docker Image") {
-                mavenNode(mavenImage: 'maven:3.5-jdk-8') {
-                    container(name: 'maven') {
-                        unstash "manifest"
-                        sh """
+                            stage("Publish Docker Image") {
+                                mavenNode(mavenImage: 'maven:3.5-jdk-8') {
+                                    container(name: 'maven') {
+                                        unstash "manifest"
+                                        sh """
                                     mvn deploy:deploy-file \
                                         -Durl=https://${mavenRepo}/repository/maven-releases \
                                         -DrepositoryId=nexus \
@@ -126,71 +149,40 @@ def call(configMap) {
                                         -Dclassifier=kubernetes \
                                         -Dfile=deployment.yaml
                                 """
-                    }
-                }
-            }
-
-            String lockName = "${env.JOB_NAME}-${env.BUILD_NUMBER}"
-
-            withLockOnMockEnvironment(lockName: lockName) {
-                stage("System test") {
-                    def prevVersion = ""
-                    clientsK8sNode(clientsImage: 'stakater/pipeline-tools:1.16.0') {
-                        container(name: 'clients') {
-                            try {
-                                prevVersion = sh(script: "kubectl -n=mock get service/${project} -o jsonpath='{.metadata.labels.version}' 2>/tmp/serr.txt", returnStdout: true).toString().trim()
-                                echo "Old version is: ${prevVersion}"
-                            } catch (err) {
-                                echo "Reading old version failed: $err"
-                                def errorMessage = readFile "/tmp/serr.txt"
-                                echo "Message: $errorMessage"
-                                if (errorMessage.contains("(NotFound)")) {
-                                    echo "Probably this is the first deployment"
-                                } else {
-                                    echo "We did not expect this!"
-                                    throw err
-                                }
-                            }
-                        }
-
-                        try {
-                            build job: "${project}-mock-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
-                            timeout(20) {
-                                mavenNode(mavenImage: 'stakater/chrome:67') {
-                                    container(name: 'maven') {
-                                        try {
-                                            git url: 'https://gitlab.com/digitaldealer/systemtest2.git',
-                                                    credentialsId: 'dd_ci',
-                                                    branch: 'master'
-
-                                            sh 'chmod +x mvnw'
-                                            sh './mvnw clean test -Dbrowser=chrome -Dheadless=true -DsuiteXmlFile=smoketest-mock.xml'
-                                        } finally {
-                                            zip zipFile: 'output.zip', dir: 'target', archive: true
-                                            archiveArtifacts artifacts: 'target/screenshots/*', allowEmptyArchive: true
-                                            junit 'target/surefire-reports/*.xml'
-                                        }
                                     }
                                 }
                             }
-                        } catch (err) {
-                            if (prevVersion != "") {
-                                // Rolling back only rolls back the deployment, the service stays
-                                // If we reapply an old manifest, the service will be correct, the replica set will be reused, but the deployment/replica set will have a new revision
-                                build job: "${project}-dev-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: prevVersion]]
-                            } else {
-                                echo "There were test failures, but there was no previous version, not rolling back mock"
+                        }
+
+                        gitlabCommitStatus(name: "System test") {
+                            systemtestStage([microservice: [name: project, version: buildVersion]], isMergeRequestBuild)
+                        }
+
+                        if (deployToDevAndProd) {
+                            stage("Deploy to dev") {
+                            build job: "${project}-dev-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
                             }
-                            throw err
+
+                            stage("Deploy to prod") {
+                                build job: "${project}-prod-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
+                            }
                         }
                     }
                 }
-            }
-
-            stage("Deploy") {
-                build job: "${project}-dev-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
-                build job: "${project}-prod-deploy", parameters: [[$class: 'StringParameterValue', name: 'VERSION', value: buildVersion]]
+            } finally {
+                if (isMergeRequestBuild) {
+                    deleteArtifactFromNexus(project, buildVersion, mavenRepo)
+                }
             }
         }
     }
+}
+
+String getBJVersion(version_base) {
+
+    int version_last = sh(
+            script: "git tag | awk -F. 'BEGIN {print \"-1\"} /v${version_base[0]}.${version_base[1]}/{print \$3}' | sort -g  | tail -1",
+            returnStdout: true
+    ) as Integer
+    return "${version_base[0]}.${version_base[1]}.${version_last + 1}"
 }
