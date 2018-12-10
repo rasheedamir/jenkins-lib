4269d00f8c18237e6bac36f5847c6e8624178cb1
Rasheed Amir <rasheed.amir@live.com>
2018-12-10 23:56:48 +0100

    Create deleteK8sClusterPipeline.groovy (#74)
    add delete cluster pipeline

---
+++ vars/deleteK8sClusterPipeline.groovy
@@ -0,0 +1,37 @@
+def call(body){
+
+    clientsK8sNode(clientsImage: 'stakater/pipeline-tools:v1.16.2') {
+        container('clients') {
+                
+            stage('Prepare') {
+                sh """
+                    mkdir cluster-tools
+                    mkdir cluster-spec
+                """
+                dir('cluster-spec'){
+                    checkout scm
+                }
+                dir('cluster-tools'){
+                    git (
+                        url: "https://gitlab.com/digitaldealer/infra/kops-cluster-scripts.git",
+                        credentialsId: 'dd_ci'
+                    )
+                }
+            }
+
+            stage('Delete') {
+                sh """
+                    mkdir ${HOME}.aws
+                    cd cluster-spec
+                    source vars.sh
+                    ../cluster-tools/prepareAWSConfig.sh
+                    ../cluster-tools/prepareTemplate.sh
+                    ../cluster-tools/exportKubecfg.sh
+                    ../cluster-tools/deleteClusterManually.sh
+                    ../cluster-tools/deleteClusterManually.sh yes
+                """        
+            }
+        }
+    }
+
+}
